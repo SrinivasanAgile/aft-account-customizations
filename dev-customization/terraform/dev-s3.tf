ae3571e037ca1a209b3def164ca176c98a2b6d5f
code-commit-git-user <srinivasan.chinnachamy@pwc.com>
2023-02-03 16:21:04 +0530

    updated s3 codebase

--- dev-customization/terraform/dev-s3.tf
+++ dev-customization/terraform/dev-s3.tf
@@ -1,9 +1,13 @@
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "mybucket0302202301"
+  bucket = "account-specific-mybucket-${random_id.suffix.dec}"
   acl    = "private"
+
   tags = {
-    Name        = "My bucket"
+    Name        = "My account-specific bucket"
     Environment = "Dev"
   }
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
